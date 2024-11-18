fd56ba81505b733be2f45e79737333aa76f28c06
Tri Ngo <tringo831@gmail.com>
2024-11-18 14:38:40 -0800

    Hash user passwords before saving to database

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     email: {
         type: String,
         required: [true, "Please enter an email"],
-        unique: true,         
+        unique: true,  
+        lowercase: true,       
         validate: [isEmail, "Please enter a valid email"]
     },
     password: {
@@ -13,6 +15,11 @@ const userSchema = mongoose.Schema({
         required: [true, "Please enter a password"],
         minlength: [6, "Password must be at least 6 characters"],
     }
+});
+
+userSchema.pre('save', async function(next) {
+    const salt = bcrypt.genSalt();
+    const password = await bcrypt.hash(this.password, salt);
 });
 
 const User = new mongoose.model('user', userSchema);
