d33190987ba5eb08b99cba99524be98068ab92fc
Tri Ngo <tringo831@gmail.com>
2024-11-18 14:32:49 -0800

    Create custom error messages for userSchema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, "Please enter an email"],
         unique: true,         
+        validate: [isEmail, "Please enter a valid email"]
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6,
+        required: [true, "Please enter a password"],
+        minlength: [6, "Password must be at least 6 characters"],
     }
 });
 
