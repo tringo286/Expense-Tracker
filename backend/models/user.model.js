3cc551f394d3073fbf188cb365e4f5e256cc2ca2
Tri Ngo <tringo831@gmail.com>
2024-11-26 22:04:25 -0800

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,6 +23,6 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
-const User = new mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
