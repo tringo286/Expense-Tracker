ea68cda1478eb2590aa4bc0dd8fc970ec506e2da
Tri Ngo <tringo831@gmail.com>
2024-11-27 17:17:42 -0800

    Set up basic login and signup forms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 const App = () => {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (form) => {
+    setCurrentForm(form);
+  };
+
   return (
     <>
-    
+      {currentForm === 'login' ? (
+        <Login onSwitch={toggleForm} />
+      ) : (
+        <Signup onSwitch={toggleForm} />
+      )}        
     </>
   );
 }
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,35 @@
+
+
+const Signup = ({ onSwitch }) => {
+  return (
+   <div className="bg-gray-100 w-screen h-screen flex justify-center items-center">
+    <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+      <h2 className="text-2xl text-gray-800 font-semibold text-center">Sign Up</h2>
+      <form>
+        <div className="mb-5">
+          <label htmlFor="email" className="block mb-1">Email</label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            className="border border-gray-300 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="password" className="block mb-1">Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            className="border border-gray-300 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        </div>
+        <button type="submit" className="w-full bg-blue-500 rounded-md py-2 text-white font-semibold hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4">Sign Up</button>
+        <p className="text-center">Already have an account? 
+        <button className="text-blue-500 hover:text-blue-700 ml-2" onClick={() => onSwitch('login')}>Log In</button>
+        </p>
+      </form>
+    </div>
+   </div>
+  );
+}
+
+export default Signup
