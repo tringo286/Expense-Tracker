db49c680f0e5c60f0a2a7a7c0e58bad2f85953a9
Tri Ngo <tringo831@gmail.com>
2025-01-07 13:54:52 -0800

    Create access and refresh token for authentication

--- backend/controllers/auth/auth.controller.js
+++ backend/controllers/auth/auth.controller.js
@@ -1,6 +1,6 @@
-const { model } = require('mongoose');
-const User = require('../models/user.model')
+const User = require('../../models/user.model')
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const handleErrors = (error) => {
     const errors = { email: "", password: ""};    
@@ -27,12 +27,6 @@ const handleErrors = (error) => {
     return errors;
 };
 
-// create json web token 
-const maxAge = 3 * 24 * 60 * 60;
-const createToken = (id) => {
-    return jwt.sign({ id }, "my secret key", { expiresIn: maxAge})
-}
-
 const getSignup = async (req, res) => {
     res.send('User sign up');
 };
@@ -43,14 +37,15 @@ const getLogin = async (req, res) => {
 
 const postSignup = async (req, res) => {
     const user = req.body;
+    const email = user.email;
     
     try {
-        const newUser = await User.create(user);
-        const token = createToken(newUser._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        const salt = await bcrypt.genSalt();
+        password = await bcrypt.hash(user.password, salt);
+        const newUser = await User.create({ email, password });    
         res.status(201).json({ success: true, user: newUser });
     } catch (error) {
-        const errors = handleErrors(error);        
+        const errors = handleErrors(error);            
         res.status(400).json({ success: false, errors: errors});
     }
 };
@@ -60,14 +55,24 @@ const postLogin = async (req, res) => {
 
     try {
         const user = await User.login(email, password);
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(200).json({ success: true, user: email});
+        
+        // Access token for short time to authenticate user for API access
+        const accessToken = jwt.sign({ email }, "my secret key", { expiresIn: '10s' });
+
+        // Refresh token for long time to obtain a new access token 
+        const refreshToken = jwt.sign({ email }, "my secret key", { expiresIn: '1h' });
+            
+        // Save refresh token with current user
+        user.refreshToken = refreshToken;
+        await user.save();
+
+        // Create secure cookie with refresh token
+        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, maxAge: 60 * 60  * 1000 });
+        res.status(200).json({ success: true, user: email, accessToken });
     } catch (err) {
-        const errors = handleErrors(err); 
+        const errors = handleErrors(err);         
         res.status(400).json({ success: false, errors: errors});
     }
-}
+};
 
 module.exports = { getSignup, getLogin, postSignup, postLogin };
-
---
+++ backend/controllers/auth/refreshToken.controller.js
@@ -0,0 +1,22 @@
+const User = require('../../models/user.model')
+const jwt = require('jsonwebtoken');
+
+const handleRefreshToken = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401); // Unauthorized 
+    const refreshToken = cookies.jwt;
+
+    const foundUser = await User.findOne({ refreshToken }).exec();
+    if (!foundUser) return res.sendStatus(403); // Forbidden 
+
+    // Evaluate jwt 
+    jwt.verify(refreshToken, "my secret key", (err, decoded) => {
+            if (err || foundUser.email !== decoded.email) return res.sendStatus(403);   
+            const email = Object.values(foundUser.email);         
+            const accessToken = jwt.sign({ email }, "my secret key", { expiresIn: '10s' });
+            res.json({ email, accessToken })
+        }
+    );
+};
+
+module.exports = { handleRefreshToken };
