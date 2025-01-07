03465ae80b0615ada93abec1081e6a8b10ea0204
Tri Ngo <tringo831@gmail.com>
2025-01-07 14:00:52 -0800

    Add refresh route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getSignup, postSignup, getLogin, postLogin } = require('../controllers/auth.controller');
+const { getSignup, postSignup, getLogin, postLogin } = require('../controllers//auth/auth.controller');
+const { handleRefreshToken } = require('../controllers//auth/refreshToken.controller')
 
 router.get('/signup', getSignup);
 router.post('/signup', postSignup);
 router.get('/login', getLogin);
 router.post('/login', postLogin);
+router.get('/refesh', handleRefreshToken);
 
 module.exports = router;
