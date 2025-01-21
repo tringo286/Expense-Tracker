147938259add2dbb6acba892e2c8afdf6a26750e
Tri Ngo <tringo831@gmail.com>
2025-01-20 18:39:29 -0800

    Refactor dashboard layout: change from flexbox to grid

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,76 +11,74 @@ const Home = () => {
     const minExpense = expenses.length ? Math.min(...expenses.map(transaction => transaction.amount)) : 0;
     
     return (
-        <section className="flex flex-col justify-center h-full">
-            <div className= "flex flex-col justify-center bg-white rounded-3xl h-full p-4 my-5">
-                <h1 className="flex text-3xl font-bold text-indigo-600">All Transactions</h1>             
-                <div className="flex justify-center gap-4">
-                    <div className="flex flex-col gap-5">
-                        <div className="bg-slate-50 rounded-2xl p-4 w-140">
-                            <Chart />
-                        </div>
-                        <div className="flex justify-around w-full">
-                            <div className="flex flex-col items-center bg-slate-50 rounded-2xl drop-shadow-xl px-8 py-4">
-                                <h3 className="text-xl text-gray-400 font-semibold">Total Income</h3>
-                                <div className="text-3xl text-lime-500 font-bold">${totalIncomes}</div>
-                            </div>
-                            <div className="flex flex-col items-center bg-slate-50 rounded-2xl drop-shadow-xl px-8 py-4">
-                                <h3 className="text-xl text-gray-400 font-semibold">Total Expenses</h3>
-                                <div className="text-3xl text-red-500 font-bold">${totalExpenses}</div>
-                            </div>
-                        </div>                   
-                    </div>               
-                    <div className="flex flex-col justify-around w-100">
-                        <div>
-                            <h2 className="text-2xl font-semibold text-indigo-600 mb-5">Recent History</h2>
-                            {transactions.length > 0 ? (
-                                <div className="flex flex-col justify-around h-40">
-                                    {transactions.slice(0, 3).map(transaction => (
-                                        <div key={transaction._id} className="flex justify-between bg-slate-50 px-4 py-2 drop-shadow-lg rounded-2xl">
-                                            <div className={transaction.type === 'expense' ? 'text-red-500' : 'text-lime-500'}>
-                                                {transaction.category.charAt(0).toUpperCase() + transaction.category.slice(1)}
-                                            </div>
-                                            <div className={transaction.type === 'expense' ? 'text-red-500' : 'text-lime-500'}>
-                                                {transaction.type === 'expense' ? `-$${transaction.amount}` : `$${transaction.amount}`}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            ) : (
-                                <p className="text-gray-500">No transactions available.</p>
-                            )}
-                        </div>
-                        <div className="text-indigo-600">
-                            <div className="flex justify-between items-center px-4">
-                                <span>Min</span>
-                                <span className="text-2xl font-semibold">Salary</span>
-                                <span>Max</span>
-                            </div> 
-                            <div className="flex justify-between bg-slate-50 drop-shadow-lg rounded-xl px-4 py-2 text-gray-600">
-                                <span>${minIncome.toLocaleString()}</span>                        
-                                <span>${maxIncome.toLocaleString()}</span>
-                            </div> 
-                        </div> 
-                        <div className="text-indigo-600">
-                            <div className="flex justify-between items-center px-4">
-                                <span>Min</span>
-                                <span className="text-2xl font-semibold">Expense</span>
-                                <span>Max</span>
-                            </div>
-                            <div className="flex justify-between bg-slate-50 drop-shadow-lg rounded-xl px-4 py-2 text-gray-600">
-                                <span>${minExpense.toLocaleString()}</span>                        
-                                <span>${maxExpense.toLocaleString()}</span>
-                            </div> 
-                        </div>
-                    </div>             
+        <section className="bg-white h-full w-full border rounded-3xl grid grid-cols-12 grid-rows-12 p-3">
+            <h1 className="col-start-1 col-end-8 row-start-1 row-end-2 flex items-center p-3 text-2xl font-semibold text-indigo-500">All Transactions</h1> 
+            <div className="col-start-1 col-end-8 row-start-2 row-end-8 p-3">
+                <div className="bg-slate-50 p-4 border border-slate-100 rounded-xl shadow-lg"><Chart /></div>
+            </div>
+            <div className="col-start-1 col-end-4 row-start-8 row-end-10 p-3">
+                <div className="bg-slate-50 border border-slate-100 rounded-xl shadow-lg h-full flex flex-col justify-center items-center">
+                    <h3 className="text-xl font-semibold text-gray-500">Total Income</h3>
+                    <p className="text-3xl font-semibold text-lime-500">${totalIncomes}</p>
+                </div>                                         
+            </div>
+            <div className="col-start-5 col-end-8 row-start-8 row-end-10 p-3">
+                <div className="bg-slate-50 border border-slate-100 rounded-xl shadow-lg h-full flex flex-col justify-center items-center">
+                    <h3 className="text-xl font-semibold text-gray-500">Total Expense</h3>
+                    <p className="text-3xl font-semibold text-red-500">${totalExpenses}</p>
+                </div>                                         
+            </div>
+            <div className="col-start-3 col-end-6 row-start-10 row-end-13 p-6">
+                <div className="bg-slate-50 border border-slate-100 rounded-xl shadow-lg h-full flex flex-col justify-center items-center">
+                    <h3 className="text-2xl font-semibold text-gray-500">Total Balance</h3>
+                    <p className="text-4xl font-semibold text-indigo-500">${totalBalance}</p>
                 </div> 
-                <div className="flex flex-row ml-56 mt-5"> 
-                    <div className="bg-slate-50 rounded-2xl drop-shadow-xl px-8 py-4">
-                        <h3 className="text-gray-400 font-semibold">Total Balance</h3>
-                        <div className="text-4xl text-indigo-600 font-bold">${totalBalance ? totalBalance.toLocaleString() : 0}</div>
-                    </div>                
-                </div>          
-            </div>     
+            </div>
+
+            <h3 className="col-start-8 col-end-13 row-start-2 row-end-3 flex justify-center items-center text-xl font-semibold text-indigo-500 ">Recent History</h3>
+
+            <div className="col-start-8 col-end-13 row-start-3 row-end-7 flex flex-col gap-y-5 p-3">
+                {transactions.length > 0 ? (
+                    transactions.slice(0, 3).map(transaction => (
+                        <div key={transaction._id} className="flex justify-between px-4 py-2 bg-slate-50 border border-slate-100 rounded-xl shadow-lg">
+                            <div className={transaction.type === 'expense' ? 'text-red-500' : 'text-lime-500'}>
+                                {transaction.category.charAt(0).toUpperCase() + transaction.category.slice(1)}
+                            </div>
+                            <div className={transaction.type === 'expense' ? 'text-red-500' : 'text-lime-500'}>
+                                {transaction.type === 'expense' ? `-$${transaction.amount}` : `$${transaction.amount}`}
+                            </div>
+                        </div>
+                    ))) : (
+                        <p className="text-center">No transactions available.</p>
+                    )              
+                }
+            </div>
+            <div className="col-start-8 col-end-13 row-start-7 row-end-9 flex flex-col justify-around">                
+                <div className="flex justify-between items-center px-3 text-indigo-500">
+                    <p>Min</p>
+                    <h3 className="text-xl font-semibold">Salary </h3>
+                    <p>Max</p>
+                </div>
+                <div className="p-3">
+                    <div className="flex justify-between items-center p-3 bg-slate-50 border border-slate-100 rounded-xl shadow-lg text-gray-500">
+                        <p>${minIncome.toLocaleString()}</p>
+                        <p>${maxIncome.toLocaleString()}</p>
+                    </div>
+                </div>                
+            </div>
+            <div className="col-start-8 col-end-13 row-start-9 row-end-11 flex flex-col justify-around ">                
+                <div className="flex justify-between items-center px-3 text-indigo-500">
+                    <p>Min</p>
+                    <h3 className="text-xl font-semibold">Epxense</h3>
+                    <p>Max</p>
+                </div>
+                <div className="p-3">
+                    <div className="flex justify-between items-center p-3 bg-slate-50 border border-slate-100 rounded-xl shadow-lg text-gray-500">
+                        <p>${minExpense.toLocaleString()}</p>
+                        <p>${maxExpense.toLocaleString()}</p>
+                    </div>
+                </div>                
+            </div>           
         </section>
     )
 }
