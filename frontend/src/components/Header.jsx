5208bbf81568fe9fa266a5e434f042377a71adb7
Tri Ngo <tringo831@gmail.com>
2025-01-31 12:35:26 -0800

    Create header for landing page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,69 +1,36 @@
-import { Link, useNavigate, useLocation} from 'react-router-dom'
-import useLogout from '../hooks/useLogout'
-import { FaChartLine } from "react-icons/fa6";
-import { FaMoneyBillTrendUp } from "react-icons/fa6";
-import { FaMoneyBillTransfer } from "react-icons/fa6";
-import { RiAdminFill } from "react-icons/ri";
-import { MdLogout } from "react-icons/md";
-import useRandomAvatar from '../hooks/useRandomAvatar';
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const logout = useLogout();           
-
-    const user = JSON.parse(localStorage.getItem('user'));
-    const email = user.email
-    const avatar = useRandomAvatar(email); 
-    const isAdmin = user && user.role === 'admin';
-    const userRole = user.role.charAt(0).toUpperCase() + user.role.slice(1)
-
-    const isDashBoardPage = location.pathname === "/";    
-    const isIncomePage = location.pathname === "/incomes";
-    const isExpensePage = location.pathname === "/expenses";
-    const isAdminPage = location.pathname === "/admin";
-
-    const signOut = async () => {
-        await logout();        
-        navigate('/login');
-    }     
-
-    return (
-      <header className="col-span-3 p-10">
-        <div className='grid grid-rows-12 bg-white h-full p-6 border rounded-3xl'>
-          <div className='row-span-2 flex flex-row items-center gap-4 mb-4'>
-            <img src={avatar} alt="User Avatar" className='w-14'/>            
-            <div>
-              <p className='text-2xl text-indigo-500 font-semibold'>{user.fullName.charAt(0).toUpperCase() + user.fullName.slice(1)}              
-              </p>
-              {isAdmin && (
-                <p className='text-sm text-gray-700'>{userRole}</p>
-              )}
-            </div>
-          </div>
-          <div className='row-span-9 flex flex-col gap-6'>
+  return (
+    <header className="w-full h-20 bg-white">
+      <div className="flex justify-between items-center h-full">
+          <Link to='/' className="flex items-center gap-2 ml-20">
+            <img 
+                src="/logo.png" 
+                alt="logo" 
+                className="object-fit h-14"
+            />
+            <h1 className="text-indigo-600 font-bold text-2xl">Expense Tracker</h1>
+          </Link> 
+        <div className="flex gap-5 mr-30 w-56">
             <Link 
-              to='/' 
-              className={`inline-flex items-center gap-x-4 text-gray-600 font-semibold hover:text-indigo-700 ${isDashBoardPage ? 'text-lg font-bold text-indigo-500' : ''}`}
+              to="/login" 
+              className="w-20 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
             >
-              <FaChartLine />
-              Dashboard
-            </Link>           
-            <Link to='/incomes' className={`inline-flex items-center gap-x-4 text-gray-600 font-semibold hover:text-indigo-700 ${isIncomePage ? 'text-lg font-bold text-indigo-500' : ''}`}><FaMoneyBillTrendUp />Incomes</Link>  
-            <Link to='/expenses' className={`inline-flex items-center gap-x-4 text-gray-600 font-semibold hover:text-indigo-700 ${isExpensePage ? 'text-lg font-bold text-indigo-500' : ''}`}><FaMoneyBillTransfer />Expenses</Link>   
-            {isAdmin && (
-              <Link to='/admin' className={`inline-flex items-center gap-x-4 text-gray-600 font-semibold hover:text-indigo-700 ${isAdminPage ? 'text-lg font-bold text-indigo-500' : ''}`}>
-                  <RiAdminFill />Admin
-              </Link>
-            )}
-          </div>
-          <div className='row-span-1 flex justify-start items-center gap-3 hover:text-indigo-700'>
-              <MdLogout />
-              <button onClick={signOut}>Log Out</button>
-          </div>  
-        </div>        
-      </header>
-    )
+              Log In
+            </Link>
+
+            <Link 
+              to="/signup" 
+              className="w-20 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+            >
+              Sign Up
+            </Link>
+        </div>
+      </div>
+    </header>   
+  )
 }
 
 export default Header
