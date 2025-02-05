909b5b9894bcfc8acfba9c3821a442d3eaa48751
Tri Ngo <tringo831@gmail.com>
2025-02-04 18:18:06 -0800

    Break down dashboard to smaller parts

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,5 +1,8 @@
 import useDataProvider from "../hooks/useDataProvider";
 import Chart from "./Chart";
+import TotalAmountCard from "./TotalAmountCard";
+import MinMaxAmountCard from "./MinMaxAmountCard";
+import TransactionCard from "./TransactionCard";
 
 const Dashboard = () => {
     const { totalExpenses, totalIncomes, totalBalance, transactions, incomes, expenses} = useDataProvider();
@@ -20,68 +23,52 @@ const Dashboard = () => {
                 <div className="bg-slate-50 p-4 border border-slate-100 rounded-xl shadow-lg"><Chart /></div>
             </div>
             <div className="col-start-1 col-end-4 row-start-8 row-end-10 p-3">
-                <div className="bg-slate-50 border border-slate-100 rounded-xl shadow-lg h-full flex flex-col justify-center items-center">
-                    <h3 className="text-xl font-semibold text-gray-500">Total Income</h3>
-                    <p className="text-3xl font-semibold text-lime-500">${totalIncomes}</p>
-                </div>                                         
+                <TotalAmountCard 
+                    title="Total Income"
+                    value={totalIncomes}                
+                    amountColor="text-lime-500"
+                />                                       
             </div>
             <div className="col-start-5 col-end-8 row-start-8 row-end-10 p-3">
-                <div className="bg-slate-50 border border-slate-100 rounded-xl shadow-lg h-full flex flex-col justify-center items-center">
-                    <h3 className="text-xl font-semibold text-gray-500">Total Expense</h3>
-                    <p className="text-3xl font-semibold text-red-500">${totalExpenses}</p>
-                </div>                                         
+                <TotalAmountCard 
+                    title="Total Expense"
+                    value={totalExpenses}                
+                    amountColor="text-red-500"
+                />                                       
             </div>
             <div className="col-start-3 col-end-6 row-start-10 row-end-13 p-6">
-                <div className="bg-slate-50 border border-slate-100 rounded-xl shadow-lg h-full flex flex-col justify-center items-center">
-                    <h3 className="text-2xl font-semibold text-gray-500">Total Balance</h3>
-                    <p className="text-4xl font-semibold text-indigo-500">${totalBalance}</p>
-                </div> 
-            </div>
-
+                <TotalAmountCard 
+                    title="Total Balance"
+                    value={totalBalance}     
+                    isPrimary={true}           
+                    amountColor="text-indigo-500"
+                />                                       
+            </div>  
             <h3 className="col-start-8 col-end-13 row-start-2 row-end-3 flex justify-center items-center text-xl font-semibold text-indigo-500 ">Recent History</h3>
 
             <div className="col-start-8 col-end-13 row-start-3 row-end-7 flex flex-col gap-y-5 p-3">
                 {transactions.length > 0 ? (
-                    transactions.slice(0, 3).map(transaction => (
-                        <div key={transaction.id} className="flex justify-between px-4 py-2 bg-slate-50 border border-slate-100 rounded-xl shadow-lg">
-                            <div className={transaction.type === 'expense' ? 'text-red-500' : 'text-lime-500'}>
-                                {transaction.category.charAt(0).toUpperCase() + transaction.category.slice(1)}
-                            </div>
-                            <div className={transaction.type === 'expense' ? 'text-red-500' : 'text-lime-500'}>
-                                {transaction.type === 'expense' ? `-$${transaction.amount}` : `$${transaction.amount}`}
-                            </div>
-                        </div>
-                    ))) : (
-                        <p className="text-center text-gray-500 text-lg">No transactions available.</p>
-                    )              
-                }
+                transactions.slice(0, 3).map(transaction => (
+                    <TransactionCard key={transaction.id} transaction={transaction} />
+                ))
+                ) : (
+                <p className="text-center text-gray-500 text-lg">No transactions available.</p>
+                )}
             </div>
             <div className="col-start-8 col-end-13 row-start-7 row-end-9 flex flex-col justify-around">                
-                <div className="flex justify-between items-center px-3 text-indigo-500">
-                    <p>Min</p>
-                    <h3 className="text-xl font-semibold">Salary </h3>
-                    <p>Max</p>
-                </div>
-                <div className="p-3">
-                    <div className="flex justify-between items-center p-3 bg-slate-50 border border-slate-100 rounded-xl shadow-lg text-gray-500">
-                        <p>${minIncome.toLocaleString()}</p>
-                        <p>${maxIncome.toLocaleString()}</p>
-                    </div>
-                </div>                
+                <MinMaxAmountCard
+                    min={minIncome}
+                    max={maxIncome}
+                    label="Income"            
+                />                   
             </div>
-            <div className="col-start-8 col-end-13 row-start-9 row-end-11 flex flex-col justify-around ">                
-                <div className="flex justify-between items-center px-3 text-indigo-500">
-                    <p>Min</p>
-                    <h3 className="text-xl font-semibold">Expense</h3>
-                    <p>Max</p>
-                </div>
-                <div className="p-3">
-                    <div className="flex justify-between items-center p-3 bg-slate-50 border border-slate-100 rounded-xl shadow-lg text-gray-500">
-                        <p>${minExpense.toLocaleString()}</p>
-                        <p>${maxExpense.toLocaleString()}</p>
-                    </div>
-                </div>                
-            </div>                      
+            <div className="col-start-8 col-end-13 row-start-9 row-end-11 flex flex-col justify-around "> 
+                <MinMaxAmountCard
+                    min={minExpense}
+                    max={maxExpense}
+                    label="Expense"            
+                />    
+            </div>                  
         </section>
     )
 }
