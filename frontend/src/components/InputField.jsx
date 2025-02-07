27a4bdaf1c965bc17195c5020684579c9535353a
Tri Ngo <tringo831@gmail.com>
2025-02-07 12:20:20 -0800

    Create input field component for income and expense pages

---
+++ frontend/src/components/InputField.jsx
@@ -0,0 +1,32 @@
+
+const InputField = ({
+    type='text', 
+    name, 
+    id, 
+    value='', 
+    onChange,     
+    placeholder='',
+    required= true,
+    error=null, 
+    }) =>   (
+    <div>
+        <label htmlFor={name}></label>        
+        <input 
+            type={type}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            className='bg-slate-50 border border-slate-100 rounded-xl shadow-lg px-2 py-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            required={required}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}                             
+        />
+        {error && (
+        <div className="text-red-500 bg-red-100 border border-red-500 rounded-md py-1 pl-2 mt-2">
+          {error}
+        </div>
+      )}
+    </div>
+  );
+
+export default InputField;
