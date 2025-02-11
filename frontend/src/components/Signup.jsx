3b323d88eb5d4a735e62d2942a9133265a2e6f6b
Tri Ngo <tringo831@gmail.com>
2025-02-10 18:27:55 -0800

    Update signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,6 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from '../hooks/useAuth';
-import { FaEye, FaEyeSlash, FaArrowLeft } from "react-icons/fa";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import GoBackButton from "./GoBackButton";
+import AuthInputField from "./AUthInputField";
+import FormButton from "./FormButton";
+import AuthBackgroundSection from "./AuthBackGroundSecton";
+import { useState } from "react";
+import axios from '../api/axios';
 
 const Signup = () => {  
   const {
@@ -8,141 +14,119 @@ const Signup = () => {
     setFullName,     
     email, 
     setEmail, 
-    emailError,      
+    emailError, 
+    setEmailError,     
     password, 
     setPassword, 
     showPassword,
-    passwordError, 
+    passwordError,
+    setPasswordError, 
     confirmPassword,
-    setConfirmPassword,
-    showConfirmPassword,
-    confirmPasswordError,    
-    handleSignupSubmit,
-    togglePasswordVisibility,
-    toggleConfirmPasswordVisibility,
-  } = useAuth();  
+    setConfirmPassword,    
+    confirmPasswordError,   
+    setConfirmPasswordError,     
+    togglePasswordVisibility,  
+    handleGoBack,
+  } = useAuth();    
 
   const navigate = useNavigate();
-  const handleGoBack = () => {
-    navigate(-1); 
-  }
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };    
+
+  const handleSignupSubmit = async (event) => {
+    event.preventDefault();
+  
+    if (password !== confirmPassword) {
+      setConfirmPasswordError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const res = await axios.post('/signup', {
+        fullName,
+        email,
+        password
+      }, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+  
+      const data = res.data;        
+  
+      if (data.user) {                                                 
+        return navigate('/login');
+      }
+  
+    } catch (error) {
+      console.error("An error occurred during signup:");
+  
+      if (error.response) {
+        console.log("Error response data:", error.response.data);         
+        setEmailError(error.response.data.errors?.email);
+        setPasswordError(error.response.data.errors?.password);
+      }       
+    }
+  };
 
   return (
-    <section className="h-screen w-screen grid grid-cols-12">
-      <div className="relative col-span-6">
-        <img src="/auth-bg.avif" alt="Login Background" className="absolute inset-0 h-full w-full object-cover"/>
-        <div className="absolute inset-0 bg-black opacity-20"></div> {/* Dark overlay */}        
-        <div className="absolute w-full h-full flex justify-center items-end">
-            <div className="text-white p-12 space-y-5">
-              <h1 className="text-4xl font-bold">Welcome to Expense Tracker</h1>
-              <p className="text-lg">Monitor your expenses, set budgets, and gain insights into your financial habits. Stay on top of your finances with ease and take control of your future.</p>
-            </div>
-        </div>
-      </div>
+    <section className="h-screen w-screen grid grid-cols-12">     
+      <AuthBackgroundSection bgImage="/auth-bg.avif" title="Welcome to Expense Tracker" description="Monitor your expenses, set budgets, and gain insights into your financial habits. Stay on top of your finances with ease and take control of your future."/>
       <div className="col-span-6 flex justify-center items-center bg-gray-50">
         <div className="bg-white p-12 border-none rounded-xl shadow-xl">
-          <div className="flex justify-between items-center mb-4">
-            <button 
-              onClick={handleGoBack} 
-              className="p-2 rounded-full text-indigo-600 hover:bg-indigo-100"
-              aria-label="Go Back"
-            >
-              <FaArrowLeft size={20} />
-            </button>
-            <div></div>
-          </div>
+          <GoBackButton onGoBack={handleGoBack}/>  
           <h2 className="text-2xl font-bold mb-2 text-indigo-600">Sign Up</h2>
           <p className="text-gray-500 mb-8">to continue to Expense Tracker</p>
-          <form onSubmit={handleSignupSubmit}>
-            <div className="mb-5 w-80">
-              <label htmlFor="fullName"></label>
-              <input 
-                type="text" 
-                name='fullName'
-                id='fullName'
-                className="border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                required
-                placeholder="Enter your full name"
-              />
-            </div>
-            <div className="mb-5">
-              <label htmlFor="email"></label>
-              <input 
-                type="email" 
-                name="email" 
-                id="email" 
-                className="border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                placeholder="Enter your email"
-              />            
-              {emailError && 
-                <div 
-                  className="text-red-500 bg-red-100 border border-red-500 rounded-md py-1 pl-2 mt-2">
-                  {emailError}
-                </div>}
-            </div>
-            <div className="mb-5 relative">
-              <label htmlFor="password"></label>
-              <input 
-                type={showPassword ? "text" : "password"} 
-                name="password" 
-                id="password" 
-                className="border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm" 
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required 
-                placeholder="Enter your password"
-              />
-              <span
-                className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                onClick={togglePasswordVisibility}
-                role="button"
-                aria-label="Toggle password visibility"
-              >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </span>
-              {passwordError && 
-                <div 
-                  className="text-red-500 bg-red-100 border border-red-500 rounded-md py-1 pl-2 mt-2">
-                  {passwordError}
-                </div>}
-            </div>
-            <div className="mb-5 relative">
-              <label htmlFor="confirmPassword"></label>
-                <input 
-                  type={showConfirmPassword ? "text" : "password"} 
-                  name="confirmPassword" 
-                  id="confirmPassword" 
-                  className="border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm" 
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required 
-                  placeholder="Confirm your password  "
-                />
-                <span
-                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                  onClick={toggleConfirmPasswordVisibility}
-                  role="button"
-                  aria-label="Toggle confirm password visibility"
-                >
-                  {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
-                </span>
-                {confirmPasswordError && 
-                  <div 
-                    className="text-red-500 bg-red-100 border border-red-500 rounded-md py-1 pl-2 mt-2">
-                    {confirmPasswordError}
-                  </div>}
-            </div>
-            <button 
-              type="submit" 
-              className="w-full bg-indigo-500 rounded-md py-2 text-white font-semibold hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4"
-              >
-                Sign Up
-            </button>
+          <form onSubmit={handleSignupSubmit} className="w-80">        
+            <label htmlFor="fullName"></label>              
+            <AuthInputField             
+              name="fullName" 
+              id="fullName" 
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              placeholder="Enter your full name"              
+            />
+          
+            <label htmlFor="email"></label>              
+            <AuthInputField
+              type="email"
+              name="email" 
+              id="email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              error={emailError}
+            />
+            
+            <label htmlFor="password"></label>              
+            <AuthInputField
+              type={showPassword ? "text" : "password"}
+              name="password" 
+              id="password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              error={passwordError}
+              icon={showPassword ? <FaEyeSlash /> : <FaEye />}
+              toggleVisibility={togglePasswordVisibility}
+            />           
+
+            <label htmlFor="confirmPassword"></label>              
+            <AuthInputField
+              type={showConfirmPassword ? "text" : "password"}
+              name="confirmPassword" 
+              id="confirmPassword" 
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Confirm your password"
+              error={confirmPasswordError}
+              icon={showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+              toggleVisibility={toggleConfirmPasswordVisibility}
+            /> 
+           <FormButton text="Sign Up"/>
             <p className="text-center">Already have an account?         
               <Link to="/login" className="text-indigo-500 hover:text-indigo-700 ml-2">Log In</Link>
             </p>
@@ -153,4 +137,4 @@ const Signup = () => {
   );
 }
 
-export default Signup
+export default Signup;
