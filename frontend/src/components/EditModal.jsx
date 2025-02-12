ce3659ce374376c3b23ec165ba65fa328b9d875f
Tri Ngo <tringo831@gmail.com>
2025-02-11 16:59:09 -0800

    Update EditModal

--- frontend/src/components/EditModal.jsx
+++ frontend/src/components/EditModal.jsx
@@ -1,26 +1,19 @@
 import { IoCloseSharp } from "react-icons/io5";
 import { forwardRef } from 'react';
-import InputField from "./InputField";
 import { toast } from 'react-toastify';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "../api/axios";
-import useIncomeProvider from "../hooks/useIncomeProvider";
+import InputField from "../components/InputFields/InputField";
 import useDataProvider from "../hooks/useDataProvider";
 
-const EditModal = ({ title, income }) => {
-    const {         
-        fetchIncomes
-    } = useDataProvider();
-
+const EditModal = ({ title, dataType, fetchData, toggleModal, setIsModalOpen, currentEditData }) => {   
     const { 
-        editIncomeCategory, setEditIncomeCategory,
-        editIncomeDescription, setEditIncomeDescription,
-        editIncomeAmount, setEditIncomeAmount,
-        editIncomeDate, setEditIncomeDate,
-        toggleEditIncomeForm,
-        setIsEditIncomeFormOpen        
-    } = useIncomeProvider();    
+        editCategory, setEditCategory,
+        editDescription, setEditDescription,
+        editAmount, setEditAmount,
+        editDate, setEditDate,
+     } = useDataProvider();
 
     const DatePickerCustomInput = forwardRef(
         ({ value, onClick, className }, ref) => (
@@ -30,30 +23,30 @@ const EditModal = ({ title, income }) => {
         ),  
     );
 
-    const handleUpdateIncomeSubmit = (e, incomeId) => {
+    const handleUpdateSubmit = (e, dataId) => {
         e.preventDefault();
         
-        const updatedIncome = {             
-            incomeCategory: editIncomeCategory,
-            incomeDescription: editIncomeDescription,
-            incomeAmount: editIncomeAmount, 
-            incomeDate: editIncomeDate 
+        const updatedData = {             
+            [`${dataType}Category`]: editCategory,
+            [`${dataType}Description`]: editDescription,
+            [`${dataType}Amount`]: editAmount, 
+            [`${dataType}Date`]: editDate 
         };
-        updateIncome(incomeId, updatedIncome);
-        setIsEditIncomeFormOpen(false); // Close the update user form after sucessfully updating
-        setEditIncomeCategory('');
-        setEditIncomeDescription('');
-        setEditIncomeAmount('');
-        setEditIncomeDate(new Date()); 
-        toast.success('Income Updated Successfully');
+        updateData(dataId, updatedData);
+        setIsModalOpen(false); // Close the update user form after sucessfully updating
+        setEditCategory('');
+        setEditDescription('');
+        setEditAmount('');
+        setEditDate(new Date()); 
+        toast.success(`${dataType} Updated Successfully`);
     };
     
-    const updateIncome = async (incomeId, updatedIncome) => {
+    const updateData = async (dataId, updatedData) => {
         try {            
-            await axios.put(`/income/${incomeId}`, updatedIncome);
-            fetchIncomes();                       
+            await axios.put(`/${dataType}/${dataId}`, updatedData);
+            fetchData();                       
         } catch (error) {            
-            console.error("There was an error updating the income", error);
+            console.error(`There was an error updating ${dataType}`, error);
         }
     };  
 
@@ -66,33 +59,33 @@ const EditModal = ({ title, income }) => {
                 <div className='bg-white p-6 rounded-lg shadow-lg w-96'>
                     <div className='flex items-center justify-between mb-5 px-3'>
                         <h2 className='text-center text-xl font-bold text-indigo-600'>{title}</h2>                
-                        <button className='text-2xl text-gray-600' onClick={toggleEditIncomeForm}><IoCloseSharp /></button>
+                        <button className='text-2xl text-gray-600' onClick={toggleModal}><IoCloseSharp /></button>
                     </div>
-                    <form onSubmit={(e) => handleUpdateIncomeSubmit(e, income._id)}>                 
+                    <form onSubmit={(e) => handleUpdateSubmit(e, currentEditData._id)}>                 
                         <InputField 
                             name="category"                            
-                            value={editIncomeCategory}
-                            onChange={setEditIncomeCategory}
-                            placeholder='Enter a new income title'          
+                            value={editCategory}
+                            onChange={setEditCategory}
+                            placeholder={`Enter a new ${dataType} title`}       
                             className='border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm mb-7'                  
                         />
                         <InputField 
                             name="description"                            
-                            value={editIncomeDescription}
-                            onChange={setEditIncomeDescription}
-                            placeholder='Enter a new income description'          
+                            value={editDescription}
+                            onChange={setEditDescription}
+                            placeholder={`Enter a new ${dataType} description`}         
                             className='border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm mb-7'                  
                         />                
                         <InputField 
                             name="amount"                            
-                            value={editIncomeAmount}
-                            onChange={setEditIncomeAmount}
-                            placeholder='Enter a new income amount'          
+                            value={editAmount}
+                            onChange={setEditAmount}
+                            placeholder={`Enter a new ${dataType} amount`}            
                             className='border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm mb-7'                  
                         /> 
                         <DatePicker
-                            selected={editIncomeDate}
-                            onChange={(date) => setEditIncomeDate(date)}
+                            selected={editDate}
+                            onChange={(date) => setEditDate(date)}
                             customInput={<DatePickerCustomInput className="mb-10 border-b-2 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:rounded-sm text-left"/>}
                         />                                
                         <button 
