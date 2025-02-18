61c3634880ea9ac42ece898ce25a50527f3d50a9
Tri Ngo <tringo831@gmail.com>
2025-02-18 15:49:13 -0800

    Add mobile view for landing page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,38 +1,77 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="w-full h-20 bg-white">
+    <header className="w-full h-24 sm:h-20 bg-white mb-5">
       <div className="flex justify-between items-center h-full">
-          <Link to='/' className="flex items-center gap-2 ml-20">
-            <img 
-                src="/logo.png" 
-                alt="logo" 
-                className="object-fit h-14"
-            />
-            <h1 className="text-indigo-600 font-bold text-2xl">Expense Tracker</h1>
-          </Link> 
-        <div className="flex gap-5 mr-30 w-96 justify-center">
+
+        <Link to='/' className="flex items-center gap-2 ml-10">
+          <img 
+              src="/logo.png" 
+              alt="logo" 
+              className="object-fit h-12 sm:h-14"
+          />
+          <h1 className="text-indigo-600 font-bold text-lg sm:text-2xl">Expense Tracker</h1>
+        </Link> 
+
+        <div className="hidden sm:flex sm:gap-5 sm:mr-30 sm:w-96 sm:justify-center">
+          <Link 
+            to="/dashboard" 
+            className="px-4 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+          >
+            Dash Board
+          </Link>
+          <Link 
+            to="/login" 
+            className="w-20 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+          >
+            Log In
+          </Link>
+
+          <Link 
+            to="/signup" 
+            className="w-20 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+          >
+            Sign Up
+          </Link>          
+          </div>
+          {/* Hamburger menu for md screens */}
+          <button 
+            className="sm:hidden p-2 text-indigo-600 focus:outline-none mr-10 text-4xl"
+            onClick={toggleMenu}
+          >
+            {isMenuOpen ? "X" : "☰"}
+          </button>
+
+          {isMenuOpen && (
+          <div className="absolute top-20 right-5 bg-white shadow-md p-4 w-48 rounded-md md:hidden">
             <Link 
               to="/dashboard" 
-              className="px-4 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+              className="block px-4 py-2 text-indigo-600 hover:bg-indigo-100"
             >
               Dash Board
             </Link>
             <Link 
               to="/login" 
-              className="w-20 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+              className="block px-4 py-2 text-indigo-600 hover:bg-indigo-100"
             >
               Log In
             </Link>
-
             <Link 
               to="/signup" 
-              className="w-20 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 block text-center"
+              className="block px-4 py-2 text-indigo-600 hover:bg-indigo-100"
             >
               Sign Up
             </Link>
-        </div>
+          </div>
+        )}
       </div>
     </header>   
   )
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -8,21 +8,22 @@ const LandingPage = () => {
             <div className="flex flex-col items-center">
                 <h1 className="text-4xl font-bold text-gray-700">Manage Your Expense</h1>
                 <h1 className="text-4xl font-bold text-indigo-600 mb-3  ">Control Your Money</h1>
-                <h2 className="text-lg font-semibold text-gray-700">Start Creating Your Budget and Save a Ton of Money</h2>
+                <h2 className="text-md sm:text-lg font-semibold text-gray-700">Start Creating Your Budget and Save a Ton of Money</h2>
                 <Link 
                     to="/login" 
-                    className="w-1/2 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-5 block text-center"
+                    className="w-1/2 bg-indigo-600 rounded-md py-2 text-white font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-5 block text-center mb-10"
                     >
                     Get Started
                 </Link>            
             </div>
-            <div className="w-[900px] h-[480px] relative flex justify-center border rounded-xl overflow-hidden shadow-2xl">
+            <div className="relative flex justify-center border rounded-xl overflow-hidden shadow-2xl">
             <img 
                 src="/demo.png" 
                 alt="Demo" 
                 className="object-fit w-full h-full"
             />
             </div>
+            <div></div>
       </section>
     )
   }
