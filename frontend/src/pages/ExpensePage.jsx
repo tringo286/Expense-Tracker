39b1a994c96960fffd6173a9c34889f88aec89d5
Tri Ngo <tringo831@gmail.com>
2025-02-25 11:53:31 -0800

    Add responsive desgin for expense page

--- frontend/src/pages/ExpensePage.jsx
+++ frontend/src/pages/ExpensePage.jsx
@@ -98,16 +98,18 @@ const ExpensePage = () => {
     );
   
     return (
-        <section className='bg-white h-[calc(100vh-80px)] w-full border rounded-3xl grid grid-cols-12 grid-rows-12 p-3'>           
-            <div className='col-start-1 col-end-13 row-start-1 row-end-4 px-3 pt-3 flex flex-col justify-around'>
-                <h1 className="flex items-center text-3xl font-semibold text-indigo-500">Expenses</h1>
-                <div className='flex justify-center items-center gap-2 py-4 bg-slate-50 border border-slate-100 rounded-xl shadow-lg'>                    
-                    <h2 className="text-2xl font-semibold text-indigo-500">Total Expense:</h2>
-                    <p className='text-red-500 text-3xl font-semibold'>-${totalExpenses}</p>
+        <section className='bg-white h-[calc(100vh-160px)] lg:h-[calc(100vh-80px)] w-full border rounded-3xl grid grid-cols-12 grid-rows-12 p-4'>   
+
+            <div className='col-start-1 col-end-13 row-start-1 row-end-3 sm:row-end-3 md:row-end-3 md:pt-3 flex flex-col justify-center gap-3 w-full'>
+                <h1 className="flex items-center text-xl md:text-3xl font-semibold text-indigo-500">Expense</h1>
+                <div className='flex justify-center items-center gap-2 py-2 sm:py-4 bg-slate-50 border border-slate-100 rounded-xl shadow-lg'>
+                    <h2 className="text-xl md:text-2xl font-semibold text-indigo-500">Total Expense:</h2>
+                    <p className='text-red-500 text-xl md:text-2xl font-semibold'>-${totalExpenses}</p>
                 </div>
             </div>
-            <div className='col-start-1 col-end-5 row-start-4 row-end-11'>
-                <form onSubmit={handleAddExpenseSubmit} className='flex flex-col gap-6 h-full p-3'>
+
+            <div className='col-span-full md:col-start-1 md:col-end-5 row-start-3 row-end-9 sm:row-end-6 md:row-end-13 sm:mt-3'>
+                <form onSubmit={handleAddExpenseSubmit} className='flex items-center md:items-start flex-col gap-3 md:gap-4 h-full w-full'>
                     <InputField 
                         name="category"                            
                         value={expenseCategory}
@@ -115,38 +117,39 @@ const ExpensePage = () => {
                         placeholder='Expense Title'                            
                     />                   
                     <InputField 
-                        name="description"                            
-                        value={expenseDescription}
-                        onChange={setExpenseDescription}
-                        placeholder='Expense Description (Optional)'       
-                        required={false}                     
+                    name="description"                            
+                    value={expenseDescription}
+                    onChange={setExpenseDescription}
+                    placeholder='Expense Description (Optional)'
+                    required={false}                            
                     />
                     <InputField 
                         name="amount"                            
                         value={expenseAmount}
                         onChange={setExpenseAmount}
-                        placeholder='Income Amount'                        
+                        placeholder='Expense Amount'
+                        className='bg-slate-50 border border-slate-100 rounded-xl shadow-lg px-2 py-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-3 sm:mb-0'
                         error={expenseAmountError}             
-                    />                  
+                    />                            
                     <div className="w-full">
                         <DatePicker
                             selected={expenseDate}
                             onChange={(date) => setExpenseDate(date)}
-                            customInput={<DatePickerCustomInput className=" text-left bg-slate-50 border border-slate-100 rounded-xl shadow-lg px-2 py-3 w-full  focus:outline-none focus:ring-2 focus:ring-indigo-500" />}
+                            customInput={<DatePickerCustomInput className=" text-left bg-slate-50 border border-slate-100 rounded-xl shadow-lg px-2 py-3 w-full  focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:mt-4 mb-2 sm:mb-4" />}
                         />
                     </div>
-                    <div className='bg-indigo-500 border rounded-full shadow-lg p-4 flex justify-center items-center gap-3 text-xl'>
+                    <div className='bg-indigo-500 border rounded-full shadow-lg p-1 md:p-2 lg:p-3 xl:p-4 flex justify-center items-center gap-3 xl:text-xl w-48 md:w-2/3'>
                         <div className='text-white'><FaPlus /></div>
                         <button
                             className='text-white'
-                            type="submit"
+                            type="submit"   
                         >
                             Add Expense
                         </button>
-                    </div>                
+                    </div>               
                 </form>
             </div>
-            <div className='col-start-5 col-end-13 row-start-4 row-end-13 flex flex-col gap-y-5 p-3 overflow-y-auto'>
+            <div className='col-span-full md:col-start-5 md:col-end-13 row-start-10 row-end-13 md:row-start-3 md:row-end-13 flex flex-col gap-y-5 p-3 overflow-y-auto sm:mt-4'>
                 {loading ? (
                     <div className="absolute inset-0 flex justify-center items-center bg-gray-50 bg-opacity-50 z-10">
                         <div className="spinner"></div>
